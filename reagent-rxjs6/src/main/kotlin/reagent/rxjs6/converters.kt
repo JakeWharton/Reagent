f275492143ad1d9fc1bce5f2e28f7390592e3008
Jake Wharton <jakew@google.com>
2018-01-16 01:01:01 -0500

    Add rudimentary converter for RxJS 6.

---
+++ reagent-rxjs6/src/main/kotlin/reagent/rxjs6/converters.kt
@@ -0,0 +1,41 @@
+package reagent.rxjs6
+
+import kotlinx.coroutines.experimental.CoroutineStart.UNDISPATCHED
+import kotlinx.coroutines.experimental.JobCancellationException
+import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.launch
+import reagent.Observable
+
+fun <I> Observable<I>.toRx(): dynamic {
+  return Rx.Observable.create<I> { observer ->
+    val job = launch(Unconfined, UNDISPATCHED) {
+      try {
+        this@toRx.subscribe {
+          observer.onNext(it)
+        }
+      } catch (ignored: JobCancellationException) {
+        return@launch
+      } catch (t: Throwable) {
+        observer.onError(t)
+        return@launch
+      }
+      observer.onComplete()
+    }
+    return@create { job.cancel() }
+  }
+}
+
+@JsModule("Rx")
+private external object Rx {
+  object Observable {
+    fun <I> create(body: (Observer<I>) -> TeardownLogic)
+  }
+
+  interface Observer<in I> {
+    fun onNext(value: I)
+    fun onComplete()
+    fun onError(t: Throwable)
+  }
+}
+
+private typealias TeardownLogic = () -> Unit
---
+++ reagent-rxjs6/src/test/kotlin/reagent/rxjs6/ObservableReagentToRxTest.kt
@@ -0,0 +1,11 @@
+package reagent.rxjs6
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class ObservableReagentToRxTest {
+  @Test fun empty() {
+    // TODO how do we test this?
+    assertTrue(true)
+  }
+}
